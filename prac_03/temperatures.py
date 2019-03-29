131d250cd5e5a6d7cb0635a9551c3b294bc6bba4
petersenryanc <ryan.petersen@my.jcu.edu.au>
2019-03-29 13:23:20 +1000

    Refactor function name for JCU Autograder

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -14,11 +14,11 @@ def main():
     while choice != "Q":
         if choice == "C":
             celsius = float(input("Celsius: "))
-            fahrenheit = convert_to_fahrenheit(celsius)
+            fahrenheit = convert__celsius_to_fahrenheit(celsius)
             print("Result: {:.2f} F".format(fahrenheit))
         elif choice == "F":
             fahrenheit = float(input("Fahrenheit: "))
-            celsius = convert_to_celsius(fahrenheit)
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
             print("Result: {:.2f} C".format(celsius))
         else:
             print("Invalid option")
@@ -27,11 +27,11 @@ def main():
     print("Thank you.")
 
 
-def convert_to_fahrenheit(celsius):
+def convert__celsius_to_fahrenheit(celsius):
     return celsius * 9.0 / 5 + 32
 
 
-def convert_to_celsius(fahrenheit):
+def convert_fahrenheit_to_celsius(fahrenheit):
     return 5 / 9 * (fahrenheit - 32)
 
 
