669cc9d347151fec36b4db0b27415b439e06d9ca
petersenryanc <ryan.petersen@my.jcu.edu.au>
2019-03-29 13:20:15 +1000

    Refactor function name for JCU Autograder

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -6,10 +6,10 @@ CP1404 - Practical 3 - broken_score
 def main():
     score = float(input("Enter score: "))
     print("Your score is {}".format(score))
-    print("That score is", check_score(score))
+    print("That score is", determine_status(score))
 
 
-def check_score(score):
+def determine_status(score):
     while score < 0 or score > 100:
         return "Invalid score. Please enter a score from 0 to 100."
     if score >= 90:
