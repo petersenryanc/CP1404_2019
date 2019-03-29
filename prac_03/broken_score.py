3ca17e608c1ff763e7770475a692bf33ca74c132
petersenryanc <ryan.petersen@my.jcu.edu.au>
2019-03-29 13:12:21 +1000

    Utilise functions for score checking

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,12 +3,22 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    print("Your score is {}".format(score))
+    print("That score is", check_score(score))
+
+
+def check_score(score):
+    while score < 0 or score > 100:
+        return "Invalid score. Please enter a score from 0 to 100."
+    if score >= 90:
+        return "excellent."
+    elif score >= 50:
+        return "passable."
+    else:
+        return "bad. Try again next time!"
+
+
+main()
