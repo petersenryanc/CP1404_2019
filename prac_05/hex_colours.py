ab7fc8e928746fc543318fc23645c03f3b7e338c
petersenryanc <ryan.petersen@my.jcu.edu.au>
2019-04-30 16:08:22 +1000

    Edit dictionary items

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,12 +2,13 @@
 CP1404 Practical 5 - Hex Colours
 """
 
-HEX_COLOURS = {"AliceBlue": "f0f8ff", "AntiqueWhite": "faebd7", "aquamarine1": "7fffd4",
-               "azure1": "f0ffff", "beige": "f5f5dc", "black": "000000", "blue1": "0000ff",
-               "brown": "a52a2a", "CadetBlue": "5f9ea0", "chocolate": "d2691e"}
+HEX_COLOURS = {"AliceBlue": "f0f8ff", "AntiqueWhite": "faebd7", "AntiqueWhite1": "ffefdb",
+               "AntiqueWhite2": "eedfcc", "AntiqueWhite3": "cdc0b0", "AntiqueWhite4": "8b8378", "aquamarine1": "7fffd4",
+               "aquamarine2": "76eec6", "aquamarine4": "458b74", "azure1": "f0ffff", "azure2": "e0eeee",
+               "azure3": "c1cdcd", "azure4": "838b8b", "beige": "f5f5dc", "bisque1": "ffe4c4"}
 
-for i, j in HEX_COLOURS.items():
-    print("{} is {}".format(i, j))
+# for i, j in HEX_COLOURS.items():
+#     print("{} is {}".format(i, j))
 
 colour = input("Enter colour: ")
 while colour != "":
