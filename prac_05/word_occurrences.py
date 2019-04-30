0e5dd51f8d98b4e16833d5ea6ed43d94a1df8baa
petersenryanc <ryan.petersen@my.jcu.edu.au>
2019-04-30 16:22:40 +1000

    First commit word_occurrences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,4 +2,17 @@
 CP1404 - Practical 5 - Word Occurrences
 """
 
-word = str(input("Text: "))
+word_dictionary = {}
+text_input = str(input("Text: "))
+words = text_input.split()
+
+for word in words:
+    num_occurrences = word_dictionary.get(word, 0)
+    word_dictionary[word] = num_occurrences + 1
+
+words = list(word_dictionary.keys())
+words.sort()
+
+max_occurrences = max((len(word) for word in words))
+for word in words:
+    print("{:{}} : {}".format(word, max_occurrences, word_dictionary[word]))
